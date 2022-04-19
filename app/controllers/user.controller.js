1e69265ba19d2a2f3d3a7ee2b448b1618cdedf3e
MinhyeSim <smine0032@gmail.com>
2022-04-19 18:23:22 +0900

    18:23

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -14,3 +14,20 @@ exports.userlist = (req,res) => {
         res.status(200).json({success : true, users})
     })    
 }
+
+exports.userlist = (req,res) => {
+    console.log(`userController access !!!`)
+    UserSchema.find()
+    .exec((err,users) => {
+        if (err) return res.status(400).send(err)
+        res.status(200).json({success : true, users})
+    })    
+}
+exports.login = (req,res) => {
+    console.log(`userController access !!!`)
+    UserSchema.find({userid: req.params.id, password: req.params/password})
+    .exec((err,users) => {
+        if (err) return res.status(400).send(err)
+        res.status(200).json({success : true, users})
+    })    
+}
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,3 +1,4 @@
-const {signup, userlist} = require('../controllers/user.controller');
+const {signup, userlist, profile, login} = require('../controllers/user.controller');
 module.exports = x => {x.app.post(`${x.url}/signup`,signup)
+                        x.app.post(`${x.url}/login`,login)
                         x.app.get(`${x.url}/list`,userlist)}
